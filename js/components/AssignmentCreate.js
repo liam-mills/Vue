0462ab196ffb6d11684245c30aedeff77b7c340f
Liam <liammills.web@gmail.com>
2022-08-12 20:31:57 +1000

    Pointer cursor for new assignment button

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -3,7 +3,7 @@ export default {
         <form @submit.prevent="add">
             <div class="border border-gray-600 rounded-lg p-3 space-x-3">
                 <input v-model="newAssignment" placeholder="New assignment..." class="p-2 rounded bg-gray-600 focus:outline-none" />
-                <input type="submit" value="Add" class="transition-all bg-blue-600 hover:bg-blue-800 rounded py-2 px-4" />
+                <input type="submit" value="Add" class="transition-all cursor-pointer bg-blue-600 hover:bg-blue-700 rounded py-2 px-4" />
             </div>
         </form>
     `,
