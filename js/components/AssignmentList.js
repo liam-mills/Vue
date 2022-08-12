6a14d0038b33647fe03f97d82db64293fb96064a
Liam <liammills.web@gmail.com>
2022-08-12 20:32:08 +1000

    Display tags in assignment list

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -5,11 +5,15 @@ export default {
     components: { Assignment },
 
     template: `
-        <section v-if="assignments.length">
+        <section v-if="assignments.length" class="space-y-3">
             <h2 class="font-bold mb-3">
                 {{ title }}
                 <span>({{ assignments.length }})</span>
             </h2>
+
+            <div class="flex gap-2">
+                <button v-for="tag in tags" class="transition-all bg-purple-600 hover:bg-purple-700 rounded px-2 py-1 text-xs">{{ tag }}</button>
+            </div>
 
             <ul class="border border-gray-600 rounded-lg divide-y divide-gray-600">
 
@@ -28,5 +32,11 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+
+    computed: {
+        tags() {
+            return ['science', 'maths', 'english']
+        }
     }
 }
