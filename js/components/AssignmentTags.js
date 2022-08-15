b98c44e0f6fb1de7ef4444f49baf336421ad45e9
Liam <liammills.web@gmail.com>
2022-08-15 21:16:17 +1000

    Fix current tag styling

--- js/components/AssignmentTags.js
+++ js/components/AssignmentTags.js
@@ -4,10 +4,12 @@ export default {
             <button 
                 @click="$emit('update:currentTag', tag)"
                 v-for="tag in allTags" 
-                class="transition-all bg-purple-600 hover:bg-purple-700 rounded px-2 py-1 text-xs"
+                class="transition-all hover:bg-purple-700 rounded px-2 py-1 text-xs"
                 :class="{
-                    'bg-blue-600': tag === currentTag
+                    'bg-blue-600': tag === currentTag,
+                    'bg-purple-600': tag != currentTag
                 }"
+                :id="tag"
             >
                 {{ tag }}
             </button>
